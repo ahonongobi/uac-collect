97b3ad7c760d967d87f945b7993c5bd1375f3299
ahonongobi <abyssiniea@gmail.com>
2022-02-24 17:39:36 +0100

    allo the form is....

---
+++ resources/views/main/formation.blade.php
@@ -0,0 +1,29 @@
+@extends('main.layout.layout')
+
+@section('content')
+ <div class="container">
+     <form action="" method="get" class="mt-5">
+         <h5>
+             Unités de formation et de Recherche impliquées 
+        </h5>
+         <div class="form-group mt-3 mb-3">
+            <label for=""></label>
+            <input type="text" name="" class="form-control" placeholder="Unités de formation et de Recherche impliquées " id="">
+         </div>
+         <button type="submit" class="btn btn-success">Enrégistrer</button>
+     </form>
+
+     <div class="container mt-3">
+        <ul>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+            <li>Liste 1</li>
+        </ul>
+     </div>
+ </div>
+@endsection
